9c10f9c22ba6d032200329fdfae18ba295f2ac4a
50ShadesOfCode <kaktymail@gmail.com>
2023-10-26 21:52:21 +0300

    Add finish user api.

--- api.py
+++ api.py
@@ -14,17 +14,27 @@ class DBProvider:
 
     def add_admin(self, tg_id):
         self.cursor.execute('UPDATE users SET admin = ? WHERE tg_id = ?', (1, tg_id))
-        
+        self.conn.commit()
 
     def if_admin(self, id):
-        pass
+        self.cursor.execute("SELECT user FROM users WHERE id = ?", (id))
+        result = self.cursor.fetchone()
+        if result.admin != 0:
+            return True
+        return False
     
+    def get_user(self, id):
+        self.cursor.execute("SELECT * FROM users WHERE id = ?", (id))
+        result = self.cursor.fetchone()
+        return result
+
     def create_user(self, user: User):
         self.cursor.execute('INSERT INTO users (name, surname, tg_id, course, group, type, admin) VALUES(?, ?, ?, ?, ?, ?)', (user.name, user.surname, user.tg_id, user.course, user.group, user.type, user.admin))
         self.conn.commit()
 
     def update_user(self, id, name):
-        pass
+        self.cursor.execute('UPDATE users SET name = ? WHERE id = ?', (name, id))
+        self.conn.commit()
     
     def remove_user(self, tg_id):
         self.cursor.execute('DELETE FROM users WHERE tg_id = ?', (tg_id))
@@ -32,16 +42,24 @@ class DBProvider:
 
     def add_task(self, task: Task):
         self.cursor.execute('INSERT INTO tasks (name, type, description, deadline, persons, creator_id, notify, completed) VALUES (?, ?, ?, ?, json(?), ?, ?, ?)', (task.name, task.type, task.description, task.deadline, task.persons, task.creator_id, task.notify, task.completed))
+        self.conn.commit()
 
     def remove_task(self, task_id):
-        pass
+        self.cursor.execute('DELETE FROM tasks WHERE id = ?', (task_id))
+        self.conn.commit()
 
-    def update_task(self, task_id, name, type, description, deadline, persons, notify):
-        pass
+    def update_task(self, task_id, field, value):
+        self.cursor.execute("UPDATE tasks SET ? = ? WHERE id = ?", (field, value, task_id))
+        self.conn.commit()
 
     def get_task(self, task_id):
-        return Task("1", "test_name", "test_type", "test_desc", datetime.now(), ["test_person_1", "test_person_2"], "test_creator", False, False)
-    
+        self.cursor.execute('SELECT * FROM tasks WHERE id = ?', (task_id))
+        result = self.cursor.fetchone()
+        return result
+
+    def get_tasks_for_user(self, user_id):
+        pass
+
     def __del__(self):
         self.conn.close()
     
